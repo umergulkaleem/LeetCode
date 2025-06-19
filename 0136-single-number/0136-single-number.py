13c8c52ca0ce3c00af32f3d15765d0480f5fd988
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2025-06-19 21:18:26 +0500

    Time: 6 ms (36.39%), Space: 19.5 MB (94.55%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,17 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # numsnew = sorted(nums)
-        # # print(numsnew)
-        # count =0
-        # for i in range(0,len(numsnew),2):
-        #     # print(i,"i")
-        #     if i<len(numsnew)-1:
-        #         if numsnew[i] != numsnew[i+1]:
-        #             return numsnew[i]
-        #     else:
-        #         return numsnew[i]
-        res = 0
-        for i in nums:
-            res = res ^ i  #xor for checing different input
-        return res
+        numsnew = sorted(nums)
+        # print(numsnew)
+        count =0
+        for i in range(0,len(numsnew),2):
+            # print(i,"i")
+            if i<len(numsnew)-1:
+                if numsnew[i] != numsnew[i+1]:
+                    return numsnew[i]
+            else:
+                return numsnew[i]
+        # res = 0
+        # for i in nums:
+        #     res = res ^ i  #xor for checing different input
+        # return res
         
