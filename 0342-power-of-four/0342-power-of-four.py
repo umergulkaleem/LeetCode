0b4cd15b4ec08f0980e68980adaf38c5e5ee95e9
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2025-06-21 14:53:43 +0500

    Time: 0 ms (100%), Space: 18 MB (17.87%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,10 +1,17 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        def helper(n,i):
-            if 4**i == n:
-                return True 
-            if i>=31:
+        # def helper(n,i):
+        #     if 4**i == n:
+        #         return True 
+        #     if i>=31:
+        #         return False
+        #     return helper(n,i+1)
+        # return helper(n,0)
+        def helper(x):
+            if x == 1:
+                return True
+            if x % 4 != 0 or x == 0:
                 return False
-            return helper(n,i+1)
-        return helper(n,0)
+            return helper(x//4)
+        return helper(n)
         
