a1cc647e40361b3101c6893d9d0bff25e2f9a0c5
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2025-06-23 17:24:16 +0500

    Time: 40 ms (59.49%), Space: 17.6 MB (64.15%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
 
-        #Recurision
+    #Recurision
         
         # num =n
         # if num == 0:
@@ -11,8 +11,10 @@ class Solution:
         # ans = self.fib(num-1)+self.fib(num-2)
         # return ans
 
+    
+    # constant space approach
+        #Space Complexity good for
 
-        #Space Complexity 
         # if n == 0:
         #     return 0
         # if n == 1:
@@ -22,15 +24,40 @@ class Solution:
         # for i in range(n):
         #     prev , curr = curr,prev+curr
         # return curr 
-        memo = {}
-        def helper(n):
-            if n == 1:
-                return 1
-            if n == 0:
-                return 0
-            if n in memo :
-                return memo[n]
-            memo[n] = helper(n-1)+helper(n-2)
-            return memo[n]
-        return helper(n)
+
+    #memoiazation
+    #from top to bottom 
+        # memo = {}
+        # def helper(n):
+        #     if n == 1:
+        #         return 1
+        #     if n == 0:
+        #         return 0
+        #     if n in memo :
+        #         return memo[n]
+        #     memo[n] = helper(n-1)+helper(n-2)
+        #     return memo[n]
+        # return helper(n)
+
+    #tabulation
+
+        if n == 0:
+            return 0
+
+        if n ==1:
+            return 1
+        array = [-1]*(n+1) #array for -1
+
+        array[0] = 1
+        array[1] = 1
+
+        for i in range(2,n+1):
+            print(i)
+            array[i] = array[i - 1] + array[i - 2]
+        print(n,"n")
+        print(array)
+        return array[n-1]
+    
+
+
             
