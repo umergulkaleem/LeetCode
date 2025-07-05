de2ed5fa02b78034d2bc58fb1f5cde2fc642b035
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2025-07-05 16:06:52 +0500

    Time: 1 ms (66.25%), Space: 18 MB (23.85%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,8 +2,6 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         count_s = Counter(s)
         count_t = Counter(t)
-        print(count_s)
-        print(count_t)
         dict1 = count_t-count_s
         return list(dict1.keys())[0]
         
