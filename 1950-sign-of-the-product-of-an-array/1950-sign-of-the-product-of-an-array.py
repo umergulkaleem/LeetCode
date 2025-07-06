498d1faa5deebf014970c1f0548fe0dc36b4a18e
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2025-07-06 17:55:18 +0500

    Time: 4 ms (7.47%), Space: 17.8 MB (89.11%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.py
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.py
@@ -1,17 +1,26 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        def helper(p,l):
-            if l == len(nums):
-                return p
-            p = p*nums[l]
-            print(p,"p")
-            return helper(p,l+1) 
-        res = helper(1,0)
-        print(res)
-        if res > 0:
-                return 1
-        elif res < 0:
-                return -1
+        # def helper(p,l):
+        #     if l == len(nums):
+        #         return p
+        #     p = p*nums[l]
+        #     print(p,"p")
+        #     return helper(p,l+1) 
+        # res = helper(1,0)
+        # print(res)
+        # if res > 0:
+        #         return 1
+        # elif res < 0:
+        #         return -1
+        # else:
+        #         return 0
+        p = 1
+        for i in nums:
+            p = p*i
+        if p>0:
+            return 1
+        elif p<0:
+            return -1
         else:
-                return 0
+            return 0
         
