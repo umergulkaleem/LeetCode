4d3c7e51f29fa2552b6b2487f3c63f69ba0e346f
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2025-07-08 20:52:44 +0500

    Time: 0 ms (100%), Space: 17.9 MB (22.43%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.py
+++ 0742-to-lower-case/0742-to-lower-case.py
@@ -1,16 +1,17 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        result =""
-        for i in range(len(s)):
-            changed = ord(s[i])
-            print(changed,"in main")
-            if changed<=90 and changed>=65:
-                changed+=32
-                print(changed)
-                character = chr(changed)
-                print(character)
+        # result =""
+        # for i in range(len(s)):
+        #     changed = ord(s[i])
+        #     print(changed,"in main")
+        #     if changed<=90 and changed>=65:
+        #         changed+=32
+        #         print(changed)
+        #         character = chr(changed)
+        #         print(character)
 
-                result+=  character
-            else:
-                result +=s[i]
-        return result
+        #         result+=  character
+        #     else:
+        #         result +=s[i]
+        # return result
+        return  s.lower()
