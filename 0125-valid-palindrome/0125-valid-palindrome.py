c9abf1340af5b110bcc8bc2a5bc2cda81af0e334
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2025-12-19 18:50:50 +0500

    Time: 20 ms (6.93%), Space: 17.9 MB (94.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newStr=""
-        for c in s:
-            if c.isalnum():
-                newStr+=c.lower()
-        return newStr ==newStr[::-1]
+        # newStr=""
+        # for c in s:
+        #     if c.isalnum():
+        #         newStr+=c.lower()
+        # return newStr ==newStr[::-1]
+
+        def helper(c):
+            return ((ord('A')<=ord(c)<=ord('Z')) or
+                    (ord('a')<=ord(c)<=ord('z')) or
+                    (ord('0')<=ord(c)<=ord('9')))
+
+        l,r = 0,len(s)-1
+        while l<r:
+            while l<r and not helper(s[l]):
+                l+=1
+            while r>l and not helper(s[r]):
+                r-=1
+            if s[l].lower()!=s[r].lower():
+                return False
+            l,r = l+1,r-1
+        return True
