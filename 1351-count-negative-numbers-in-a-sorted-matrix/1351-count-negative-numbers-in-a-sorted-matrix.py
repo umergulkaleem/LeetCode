aaf554474a1694d924f31bd5db5da2cc49fe3f83
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2025-12-29 00:41:24 +0500

    Time: 0 ms (100%), Space: 18.4 MB (87.14%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -5,14 +5,14 @@ class Solution:
         pointer =len(grid[0])-1
         for i in range(len(grid)):
             # count +=count
-            print(pointer,"pointer")
+            # print(pointer,"pointer")
             while pointer>=0 and grid[i][pointer]<0:
                 pointer-=1
             val = (len(grid[i])-1)-pointer
-            print(val,"to add")
+            # print(val,"to add")
             count +=val
-            print(grid[i])
-            print(count)
+            # print(grid[i])
+            # print(count)
         return count
         
 
