cd377721d1724fea216a6891bf8d676b830db178
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2025-12-30 17:39:05 +0500

    Time: 134 ms (20.39%), Space: 33.8 MB (15.95%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,31 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # stack = []
-        
-        # for i in range(len(temperatures)):
-        #     found = False
-        #     days = 0
 
-        #     for k in range(i+1,len(temperatures)):
-        #         days+=1
-        #         if temperatures[k]>temperatures[i]:
-        #             stack.append(days)
-        #             found = True
-            
-        #             break
-        #     if not found:
-        #         stack.append(0)
-        # return stack
-        n = len(temperatures)
-        res = [0] * n
-        stack = []  # stores indices
-
-        for i, temp in enumerate(temperatures):
-            # While current temp is warmer than the one at stack's top
-            while stack and temperatures[i] > temperatures[stack[-1]]:
-                prev_index = stack.pop()
-                res[prev_index] = i - prev_index
-            stack.append(i)
-
-        return res
+        result = [0]*len(temperatures)
+        stack = []
+        for i,t in enumerate(temperatures):
+            while stack and stack[-1][0]<t:
+                stackT,stacki = stack.pop()
+                result[stacki] = (i-stacki)
+            stack.append([t,i])
+        return result
         
