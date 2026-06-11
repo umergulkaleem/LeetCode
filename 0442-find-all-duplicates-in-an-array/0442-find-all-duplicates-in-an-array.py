df71cb3a859e46ded121c9e4b6e2c344eba1239b
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2026-06-11 19:15:45 +0500

    Time: 58 ms (7.62%), Space: 30.1 MB (45.7%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,10 +1,22 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        hashmap = Counter(nums)
-        res = []
-        for i in hashmap:
-            print(i)
-            if hashmap[i] >1:
-                res.append(i)
+        # hashmap = Counter(nums)
+        # res = []
+        # for i in hashmap:
+        #     print(i)
+        #     if hashmap[i] >1:
+        #         res.append(i)
+        # return res
+
+        nums.sort()
+        print(nums)
+        res= []
+        prev = nums[0]
+        for i in range(1,len(nums)):
+            if nums[i] == prev:
+                res.append(nums[i])
+            prev= nums[i]
+
         return res
+
         
