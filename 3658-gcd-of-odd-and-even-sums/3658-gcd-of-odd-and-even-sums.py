a5c97dc0752e2b3b0e9490e6b0248630be2adf2c
Umer Gul Kaleem <148777852+umergulkaleem@users.noreply.github.com>
2026-07-15 21:50:59 +0500

    Time: 0 ms (100%), Space: 19.3 MB (21.29%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -1,4 +1,10 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        return n
+
+
+        val1 = n*n
+        val2 = val1+n
+        return math.gcd(val1,val2)
+
+        # return n
         
